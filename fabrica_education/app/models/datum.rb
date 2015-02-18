bd226f03b6fac1526d497e4d965db0db8eb18574
minkono <min.kono.min@gmail.com>
2015-02-18 10:07:50 +0900

    uploader

--- fabrica_education/app/models/datum.rb
+++ fabrica_education/app/models/datum.rb
@@ -2,4 +2,9 @@ class Datum < ActiveRecord::Base
 	has_many :cam_data
 	belongs_to :user
 	belongs_to :printer
+
+	has_attached_file :file
+	validates_attachment_presence :file
+	validates_attachment_content_type :file, :content_type => ['application/sla']
+
 end
--- fabrica_education/db/schema.rb
+++ fabrica_education/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150216094331) do
+ActiveRecord::Schema.define(version: 20150217113721) do
 
   create_table "cam_data", force: true do |t|
     t.integer  "datum_id"
@@ -30,6 +30,10 @@ ActiveRecord::Schema.define(version: 20150216094331) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.binary   "cad_data"
+    t.string   "file_file_name"
+    t.string   "file_content_type"
+    t.integer  "file_file_size"
+    t.datetime "file_updated_at"
   end
 
   create_table "jobs", force: true do |t|
